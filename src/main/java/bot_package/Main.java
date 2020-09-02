ea1ff3f4f8e6f4ddd1e1fcc58b6e807aef197f85
kacper <kacperlipiec123@gmail.com>
2020-09-02 21:13:26 +0200

    Initial Commit

--- src/main/java/bot_package/Main.java
+++ src/main/java/bot_package/Main.java
@@ -16,6 +16,7 @@ public class Main {
            if (messageCreateEvent.getMessageContent().equalsIgnoreCase("Hello"))
            {
                messageCreateEvent.getChannel().sendMessage("World");
+
            }
         });
     }
